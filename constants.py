90e92fb6b74e61d3ec43483b70bcde3768d43e21
AbhisarAnand <abhisar.muz@gmail.com>
2021-07-22 22:11:23 -0400

    Mixed a minor bug

--- constants.py
+++ constants.py
@@ -11,4 +11,4 @@ MIN_TEMP = 22.0
 FILE_NAME = os.path.join(os.path.dirname(os.path.realpath(__file__)), "temperature.json")
 
 TIME_DAILY_UPDATE_LOW_THRESHOLD = datetime.datetime.now().replace(hour=9, minute=59, second=0, microsecond=0)
-TIME_DAILY_UPDATE_HIGH_THRESHOLD = datetime.datetime.now().replace(hour=10, minute=3, second=0, microsecond=0)
+TIME_DAILY_UPDATE_HIGH_THRESHOLD = datetime.datetime.now().replace(hour=22, minute=3, second=0, microsecond=0)
